6746ea14d76b5f06a87f0d3b7b444b5c338c0457
diaaanek <dkorongy@gmail.com>
2019-04-27 00:19:21 -0400

    set up react

---
+++ react-bot/src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+//
+// <div class="wrapper">
+//     <div class="sidebar">
+//         <h2>Weatherbot</h2>
+//         <p> Inspired to build upon what I learned at the Women Techmaker's International Women's Day workshop.
+// 					<br>
+// Building an AI chatbot with Dialogflow
+// <br>
+// Dialogflow + Socket.IO + Node JS + Bot UI
+//
+// Chatbot using Express.JS (Node.JS Framework) and Google's DialogFlow</p>
+//     </div>
+//     <div class="chat-container">
+//         <div class="top-bar">
+//             <div class="user-info"></div>
+//         </div>
+//         <div class="chat-window"></div>
+//         <div class="bottom-bar"><input class="chat-input" type="text" placeholder="" /><button class="send-btn" id="send-btn" type="submit"><i class="material-icons">favorite</i></button></div>
+//     </div>
+// </div>
+//
